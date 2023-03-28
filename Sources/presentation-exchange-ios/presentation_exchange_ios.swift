ea6d8461103fbe5e3a3eb5d1b49cb589954f75fd
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-03-28 15:01:23 +0300

    [fix] added schemas and data resources

--- Sources/presentation-exchange-ios/presentation_exchange_ios.swift
+++ Sources/presentation-exchange-ios/presentation_exchange_ios.swift
@@ -1,6 +1,13 @@
+import Foundation
+
 public struct presentation_exchange_ios {
     public private(set) var text = "Hello, World!"
 
     public init() {
+      
+//      // Get URL of config.json
+//      let configURL = Bundle.module.url(forResource: "input-descriptor", withExtension: "json")
+//      
+//      print(configURL)
     }
 }
