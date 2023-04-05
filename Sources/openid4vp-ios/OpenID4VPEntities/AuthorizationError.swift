2c68fb7c53c39943245ce31d84a15645c4156020
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-04-05 15:43:47 +0300

    [fix] added authorisation status enum

---
+++ Sources/openid4vp-ios/OpenID4VPEntities/AuthorizationError.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public enum AuthorizationError: Error {
+  case unsupportedResponseType(type: String)
+  case missingResponseType
+  case missingPresentationDefinition
+  case nonHttpsPresentationDefinitionUri
+}
