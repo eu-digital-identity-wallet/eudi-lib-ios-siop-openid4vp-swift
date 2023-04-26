ea735c9dcc12a1c5f71de8cc31a8741f3ec8b413
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-04-26 13:32:57 +0300

    [fix] public claim object

--- Sources/OpenID4VP/PresentationEntities/Claim/Claim.swift
+++ Sources/OpenID4VP/PresentationEntities/Claim/Claim.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct Claim {
   let id: ClaimId
   let jsonObject: JSONObject
+
+  public init(id: ClaimId, jsonObject: JSONObject) {
+    self.id = id
+    self.jsonObject = jsonObject
+  }
 }
