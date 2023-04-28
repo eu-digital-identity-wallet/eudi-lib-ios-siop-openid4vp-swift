d7071945a7a4d5141a38b17a862321b30d792710
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-04-28 12:29:14 +0300

    [fix] added poster to project

--- Sources/OpenID4VP/Utilities/Poster.swift
+++ Sources/OpenID4VP/Utilities/Poster.swift
@@ -1,8 +1,28 @@
-//
-//  File.swift
-//  
-//
-//  Created by TSIFLITZIS Dimitris on 27/4/23.
-//
-
 import Foundation
+
+public enum PostError: Error {
+  case invalidUrl
+  case networkError(Error)
+}
+
+public protocol Posting {
+  func post(request: URLRequest) async -> Result<Bool, PostError>
+}
+
+public struct Poster: Posting {
+  public init() {}
+  public func post(request: URLRequest) async -> Result<Bool, PostError> {
+    do {
+      let (_, _) = try await URLSession.shared.data(for: request)
+      return .success(true)
+    } catch let error as NSError {
+      if error.domain == NSURLErrorDomain {
+        return .failure(.networkError(error))
+      } else {
+        return .failure(.networkError(error))
+      }
+    } catch {
+      return .failure(.networkError(error))
+    }
+  }
+}
