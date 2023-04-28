855e6ec025ba58b2136199ed7fef790ec621916e
Tzouvaras, Stilianos <stylianos.tzouvaras@netcompany-intrasoft.com>
2023-04-28 20:13:00 +0300

    spm file cleanup

--- Package.swift
+++ Package.swift
@@ -4,82 +4,82 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenID4VP",
-    platforms: [.iOS(.v14), .macOS(.v12)],
-    products: [
-        .library(
-            name: "git@github.com:niscy-eudiw/openid4vp-ios.git",
-            targets: ["OpenID4VP"]),
-    ],
-    dependencies: [
-      .package(
-        url: "https://github.com/kylef/JSONSchema.swift",
-        from: "0.6.0"
-      ),
-      .package(
-        url: "https://github.com/KittyMac/Sextant.git",
-          .upToNextMinor(from: "0.4.0")
-      ),
-      .package(
-        url: "https://github.com/realm/SwiftLint.git",
-          .upToNextMinor(from: "0.51.0")
-      ),
-      .package(
-        url: "https://github.com/airsidemobile/JOSESwift.git",
-          .upToNextMinor(from: "2.4.0")
-      ),
-      .package(
-        url: "https://github.com/birdrides/mockingbird.git",
-          .upToNextMinor(from: "0.20.0")
-      ),
-    ],
-    targets: [
-        .target(
-            name: "OpenID4VP",
-            dependencies: [
-              .product(
-                name: "Sextant",
-                package: "Sextant"
-              ),
-              .product(
-                name: "JSONSchema",
-                package: "JSONSchema.swift"
-              ),
-              .product(
-                name: "JOSESwift",
-                package: "JOSESwift"
-              )
-            ],
-            path: "Sources",
-            resources: [
-              .process("Resources")
-            ],
-            plugins: [
-              .plugin(name: "SwiftLintPlugin", package: "SwiftLint")
-            ]
+  name: "OpenID4VP",
+  platforms: [.iOS(.v14), .macOS(.v12)],
+  products: [
+    .library(
+      name: "git@github.com:niscy-eudiw/openid4vp-ios.git",
+      targets: ["OpenID4VP"]),
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/kylef/JSONSchema.swift",
+      from: "0.6.0"
+    ),
+    .package(
+      url: "https://github.com/KittyMac/Sextant.git",
+      .upToNextMinor(from: "0.4.0")
+    ),
+    .package(
+      url: "https://github.com/realm/SwiftLint.git",
+      .upToNextMinor(from: "0.51.0")
+    ),
+    .package(
+      url: "https://github.com/airsidemobile/JOSESwift.git",
+      .upToNextMinor(from: "2.4.0")
+    ),
+    .package(
+      url: "https://github.com/birdrides/mockingbird.git",
+      .upToNextMinor(from: "0.20.0")
+    ),
+  ],
+  targets: [
+    .target(
+      name: "OpenID4VP",
+      dependencies: [
+        .product(
+          name: "Sextant",
+          package: "Sextant"
         ),
-        .testTarget(
-            name: "OpenID4VPTests",
-            dependencies: [
-              "OpenID4VP",
-              .product(
-                name: "Mockingbird",
-                package: "mockingbird"
-              ),
-              .product(
-                name: "JSONSchema",
-                package: "JSONSchema.swift"
-              ),
-              .product(
-                name: "Sextant",
-                package: "Sextant"
-              ),
-              .product(
-                name: "JOSESwift",
-                package: "JOSESwift"
-              )
-            ],
-            path: "Tests"
+        .product(
+          name: "JSONSchema",
+          package: "JSONSchema.swift"
         ),
-    ]
+        .product(
+          name: "JOSESwift",
+          package: "JOSESwift"
+        )
+      ],
+      path: "Sources",
+      resources: [
+        .process("Resources")
+      ],
+      plugins: [
+        .plugin(name: "SwiftLintPlugin", package: "SwiftLint")
+      ]
+    ),
+    .testTarget(
+      name: "OpenID4VPTests",
+      dependencies: [
+        "OpenID4VP",
+        .product(
+          name: "Mockingbird",
+          package: "mockingbird"
+        ),
+        .product(
+          name: "JSONSchema",
+          package: "JSONSchema.swift"
+        ),
+        .product(
+          name: "Sextant",
+          package: "Sextant"
+        ),
+        .product(
+          name: "JOSESwift",
+          package: "JOSESwift"
+        )
+      ],
+      path: "Tests"
+    ),
+  ]
 )
