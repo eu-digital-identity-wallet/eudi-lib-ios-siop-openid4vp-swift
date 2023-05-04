e66f78afd35e3e0d70601618e85a268ce4113c41
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-05-04 13:32:19 +0300

    [fix] updated presentation exchange lib

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/niscy-eudiw/presentation-exchange-swift.git",
-      .upToNextMinor(from: "0.0.2")
+      .upToNextMinor(from: "0.0.3")
     )
   ],
   targets: [
