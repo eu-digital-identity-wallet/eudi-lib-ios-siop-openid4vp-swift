530fa59c8420bc5159fb456f321e70db40424fc0
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-05-08 12:55:22 +0300

    [fix] updated response mode

--- Sources/OpenID4VPEntities/ResponseMode.swift
+++ Sources/OpenID4VPEntities/ResponseMode.swift
@@ -2,5 +2,26 @@ import Foundation
 
 public enum ResponseMode {
   case directPost(responseURI: URL)
+  case directPostJWT(responseURI: URL)
+  case query(responseURI: URL)
+  case fragment(responseURI: URL)
   case none
+}
+
+extension ResponseMode {
+  init(authorizationRequestData: AuthorizationRequestUnprocessedData) throws {
+
+    guard
+      let responseType = authorizationRequestData.responseMode
+    else {
+      self = .none
+      return
+    }
+
+    do {
+      self = try ResponseMode(authorizationRequestData: authorizationRequestData)
+    } catch {
+      throw ValidatedAuthorizationError.unsupportedResponseMode(authorizationRequestData.responseType)
+    }
+  }
 }
