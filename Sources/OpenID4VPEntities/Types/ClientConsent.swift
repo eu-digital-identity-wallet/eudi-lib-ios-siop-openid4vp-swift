c339dd67b6766ea443d2b59296be958221baa2ca
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-05-22 13:54:55 +0300

    [fix] updated consent enum

--- Sources/OpenID4VPEntities/Types/ClientConsent.swift
+++ Sources/OpenID4VPEntities/Types/ClientConsent.swift
@@ -1,9 +1,35 @@
 import Foundation
 import PresentationExchange
 
+/**
+ Consent. Holder decided to respond to a request
+  */
 enum ClientConsent {
-  case idTokenConsensus
+
+  /**
+   In response to a SiopAuthentication, Holder/Wallet provides a JWT
+    - Parameters:
+     - idToken: The id_token produced by the wallet
+    */
+  case idTokenConsensus(idToken: JWTString)
+
+  /**
+   In response to an OpenId4VPAuthorization where the wallet has claims that fulfill Verifier's presentation definition
+    - Parameters:
+     - approvedClaims: The claims to include chosen by the holder
+    */
   case vpTokenConsensus(approvedClaims: [Claim])
+
+  /**
+   In response to a SiopOpenId4VPAuthentication
+    - Parameters:
+     - idToken: The id_token produced by the wallet
+     - approvedClaims: The claims to include chosen by the holder
+    */
   case idAndVPTokenConsensus(approvedClaims: [Claim])
+
+  /**
+   No consensus. Holder decided to reject the request
+    */
   case negative
 }
