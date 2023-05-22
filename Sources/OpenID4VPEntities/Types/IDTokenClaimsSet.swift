712b1c39b771f004f64c42d907c0ab0c46949970
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-05-22 15:55:37 +0300

    [fix] added IDTokenClaimsSet to project

---
+++ Sources/OpenID4VPEntities/Types/IDTokenClaimsSet.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct IDTokenClaimsSet: Codable {
+  let iss: String
+  let sub: String
+  let aud: String
+  let nonce: String
+  let exp: Int
+  let iat: Int
+  let authTime: Int
+  let acr: String
+  let atHash: String
+  
+  enum CodingKeys: String, CodingKey {
+    case iss
+    case sub
+    case aud
+    case nonce
+    case exp
+    case iat
+    case authTime = "auth_time"
+    case acr
+    case atHash = "at_hash"
+  }
+}
