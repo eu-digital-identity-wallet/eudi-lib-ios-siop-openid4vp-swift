f641aa3b31283dda05d0b6a1cccdb3bcb0fb241a
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2023-06-28 17:17:20 +0300

    [fix] restored workflow file

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
       .upToNextMinor(from: "0.20.0")
     ),
     .package(
-      url: "https://dtsiflit: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@github.com/niscy-eudiw/eudi-lib-ios-presentation-exchange-swift.git",
+      url: "https://github.com/niscy-eudiw/eudi-lib-ios-presentation-exchange-swift.git",
       .upToNextMinor(from: "0.0.21")
     )
   ],
