c36e35e9212cb1786092e507eb1b4b93f597f74f
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2024-03-04 12:54:58 +0200

    [fix] updated client meta data, made fields mostly optional

--- Sources/Entities/ClientMetaData/ClientMetaData.swift
+++ Sources/Entities/ClientMetaData/ClientMetaData.swift
@@ -83,34 +83,34 @@ public struct ClientMetaData: Codable, Equatable {
     let jwks = metaData["jwks"] as? [String: Any]
     self.jwks = jwks?.toJSONString()
 
-    self.idTokenSignedResponseAlg = try metaData.getValue(
+    self.idTokenSignedResponseAlg = try? metaData.getValue(
       for: "id_token_signed_response_alg",
       error: ValidatedAuthorizationError.invalidClientMetadata
     )
-    self.idTokenEncryptedResponseAlg = try metaData.getValue(
+    self.idTokenEncryptedResponseAlg = try? metaData.getValue(
       for: "id_token_encrypted_response_alg",
       error: ValidatedAuthorizationError.invalidClientMetadata
     )
-    self.idTokenEncryptedResponseEnc = try metaData.getValue(
+    self.idTokenEncryptedResponseEnc = try? metaData.getValue(
       for: "id_token_encrypted_response_enc",
       error: ValidatedAuthorizationError.invalidClientMetadata
     )
-    self.subjectSyntaxTypesSupported = try metaData.getValue(
+    self.subjectSyntaxTypesSupported = (try? metaData.getValue(
       for: "subject_syntax_types_supported",
       error: ValidatedAuthorizationError.invalidClientMetadata
-    )
+    )) ?? []
 
-    self.authorizationSignedResponseAlg = try metaData.getValue(
+    self.authorizationSignedResponseAlg = try? metaData.getValue(
       for: "authorization_signed_response_alg",
       error: ValidatedAuthorizationError.invalidClientMetadata
     )
 
-    self.authorizationEncryptedResponseAlg = try metaData.getValue(
+    self.authorizationEncryptedResponseAlg = try? metaData.getValue(
       for: "authorization_encrypted_response_alg",
       error: ValidatedAuthorizationError.invalidClientMetadata
     )
 
-    self.authorizationEncryptedResponseEnc = try metaData.getValue(
+    self.authorizationEncryptedResponseEnc = try? metaData.getValue(
       for: "authorization_encrypted_response_enc",
       error: ValidatedAuthorizationError.invalidClientMetadata
     )
