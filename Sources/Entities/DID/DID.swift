9dc34d762fc28f978e9f710e09937607b69cd292
tsiflitzis <tsiflitzis@gmail.com>
2024-06-20 14:51:35 +0300

    [fix] added DID strcuts to project

---
+++ Sources/Entities/DID/DID.swift
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Foundation
+
+private let DID_URL_SYNTAX = try? NSRegularExpression(pattern: "^did:[a-z0-9]+:(([A-Z.a-z0-9]|-|_|%[0-9A-Fa-f][0-9A-Fa-f])*:)*([A-Z.a-z0-9]|-|_|%[0-9A-Fa-f][0-9A-Fa-f])+(/(([-A-Z._a-z0-9]|~)|%[0-9A-Fa-f][0-9A-Fa-f]|([!$&'()*+,;=])|:|@)*)*(\\?(((([-A-Z._a-z0-9]|~)|%[0-9A-Fa-f][0-9A-Fa-f]|([!$&'()*+,;=])|:|@)|/|\\?)*))?(#(((([-A-Z._a-z0-9]|~)|%[0-9A-Fa-f][0-9A-Fa-f]|([!$&'()*+,;=])|:|@)|/|\\?)*))?$", options: [])
+private let DID_SYNTAX = try? NSRegularExpression(pattern: "^did:[a-z0-9]+:(([A-Z.a-z0-9]|-|_|%[0-9A-Fa-f][0-9A-Fa-f])*:)*([A-Z.a-z0-9]|-|_|%[0-9A-Fa-f][0-9A-Fa-f])+$", options: [])
+
+public struct AbsoluteDIDUrl {
+  private let uri: URL
+
+  private init(uri: URL) {
+    self.uri = uri
+  }
+
+  var string: String {
+    return uri.absoluteString
+  }
+
+  public static func parse(_ string: String) -> AbsoluteDIDUrl? {
+    guard let regex = DID_URL_SYNTAX else {
+      return nil
+    }
+
+    let parsed = DID.parse(string)
+    if regex.matches(
+      in: string,
+      options: [],
+      range: NSRange(
+        location: 0, length: string.utf16.count
+      )
+    ).isEmpty == false && parsed != nil {
+      if let url = URL(string: string) {
+        return AbsoluteDIDUrl(uri: url)
+      } else {
+        return nil
+      }
+    } else {
+      return nil
+    }
+  }
+}
+
+public struct DID {
+
+  public let uri: URL
+
+  public init(uri: URL) {
+    self.uri = uri
+  }
+
+  public var string: String {
+    return uri.absoluteString
+  }
+
+  public static func parse(_ string: String) -> DID? {
+    guard
+      let regex = DID_SYNTAX,
+      regex.matches(
+        in: string,
+        options: [],
+        range: NSRange(
+            location: 0,
+            length: string.utf16.count
+        )
+      ).isEmpty == false,
+      let url = URL(string: string)
+    else {
+      return nil
+    }
+
+    return DID(uri: url)
+  }
+}
