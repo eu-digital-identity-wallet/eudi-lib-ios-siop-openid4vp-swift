64e52975c2f3b5488b89c3117551fabf526ba06b
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-01-28 16:30:31 +0200

    [fix] new spec constants

---
+++ Sources/Entities/Types/OpenId4VPSpec.swift
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public struct OpenId4VPSpec {
+  public static let clientIdSchemeSeparator: Character = ":"
+  public static let clientIdSchemePreRegistered = "pre-registered"
+  public static let clientIdSchemeRedirectUri = "redirect_uri"
+  public static let clientIdSchemeHttps = "https"
+  public static let clientIdSchemeDid = "did"
+  public static let clientIdSchemeX509SanUri = "x509_san_uri"
+  public static let clientIdSchemeX509SanDns = "x509_san_dns"
+  public static let clientIdSchemeVerifierAttestation = "verifier_attestation"
+}
+
