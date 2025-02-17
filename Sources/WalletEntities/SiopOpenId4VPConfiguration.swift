716c07ba23466a8d1bd53b76289f74748703d30e
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-02-17 14:05:27 +0200

    [fix] updated vp configuration

--- Sources/WalletEntities/SiopOpenId4VPConfiguration.swift
+++ Sources/WalletEntities/SiopOpenId4VPConfiguration.swift
@@ -15,6 +15,29 @@
  */
 import Foundation
 
+public struct SupportedTransactionDataType: Codable {
+  public let type: TransactionDataType
+  public let hashAlgorithms: Set<HashAlgorithm>
+  
+  public init(type: TransactionDataType, hashAlgorithms: Set<HashAlgorithm>) throws {
+    guard !hashAlgorithms.isEmpty else {
+      throw NSError(domain: "SupportedTransactionDataTypeError", code: 1, userInfo: [NSLocalizedDescriptionKey: "hashAlgorithms cannot be empty"])
+    }
+    guard hashAlgorithms.contains(HashAlgorithm.sha256) else {
+      throw NSError(domain: "SupportedTransactionDataTypeError", code: 2, userInfo: [NSLocalizedDescriptionKey: "'sha-256' must be a supported hash algorithm"])
+    }
+    self.type = type
+    self.hashAlgorithms = hashAlgorithms
+  }
+  
+  public static func `default`() -> SupportedTransactionDataType {
+    try! .init(
+      type: .init(value: "transaction_data"),
+      hashAlgorithms: .init([.sha256])
+    )
+  }
+}
+
 public struct SiopOpenId4VPConfiguration {
   public let subjectSyntaxTypesSupported: [SubjectSyntaxType]
   public let preferredSubjectSyntaxType: SubjectSyntaxType
@@ -29,7 +52,8 @@ public struct SiopOpenId4VPConfiguration {
   public let jarConfiguration: JARConfiguration
   public let vpConfiguration: VPConfiguration
   public let session: Networking
-
+  public let supportedTransactionData: SupportedTransactionDataType
+  
   public init(
     subjectSyntaxTypesSupported: [SubjectSyntaxType],
     preferredSubjectSyntaxType: SubjectSyntaxType,
@@ -43,7 +67,8 @@ public struct SiopOpenId4VPConfiguration {
     knownPresentationDefinitionsPerScope: [String: PresentationDefinition] = [:],
     jarConfiguration: JARConfiguration = .default,
     vpConfiguration: VPConfiguration = VPConfiguration.default(),
-    session: Networking = Self.walletSession
+    session: Networking = Self.walletSession,
+    supportedTransactionData: SupportedTransactionDataType = SupportedTransactionDataType.default()
   ) {
     self.subjectSyntaxTypesSupported = subjectSyntaxTypesSupported
     self.preferredSubjectSyntaxType = preferredSubjectSyntaxType
@@ -58,8 +83,9 @@ public struct SiopOpenId4VPConfiguration {
     self.jarConfiguration = jarConfiguration
     self.vpConfiguration = vpConfiguration
     self.session = session
+    self.supportedTransactionData = supportedTransactionData
   }
-
+  
   internal init() throws {
     subjectSyntaxTypesSupported = []
     preferredSubjectSyntaxType = .decentralizedIdentifier
@@ -74,16 +100,17 @@ public struct SiopOpenId4VPConfiguration {
     jarConfiguration = .default
     vpConfiguration = VPConfiguration.default()
     session = URLSession.shared
+    supportedTransactionData = SupportedTransactionDataType.default()
   }
-
+  
   public static let walletSession: Networking = {
     /*let delegate = SelfSignedSessionDelegate()
-    let configuration = URLSessionConfiguration.default
-    return URLSession(
-      configuration: configuration,
-      delegate: delegate,
-      delegateQueue: nil
-    )*/
+     let configuration = URLSessionConfiguration.default
+     return URLSession(
+     configuration: configuration,
+     delegate: delegate,
+     delegateQueue: nil
+     )*/
     URLSession.shared
   }()
 }
