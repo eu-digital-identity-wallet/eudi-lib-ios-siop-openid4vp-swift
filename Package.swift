e3382c0c6ae883390c18383500cd107887b6eca1
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-02-19 09:27:00 +0200

    [fix] updated presentation exchange

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-presentation-exchange-swift.git",
-      .upToNextMajor(from: "0.2.1")
+      .upToNextMajor(from: "0.3.0")
     ),
     .package(
       url: "https://github.com/apple/swift-certificates.git",
