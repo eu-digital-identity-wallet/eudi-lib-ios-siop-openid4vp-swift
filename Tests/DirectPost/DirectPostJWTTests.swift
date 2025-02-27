63406233eea298c4ab815902f83a2490f0245538
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-02-27 11:22:52 +0200

    [wip] dcql example

--- Tests/DirectPost/DirectPostJWTTests.swift
+++ Tests/DirectPost/DirectPostJWTTests.swift
@@ -192,7 +192,6 @@ final class DirectPostJWTTests: DiXCTest {
       
       // Obtain consent
       let submission = TestsConstants.presentationSubmission(presentationDefinition!)
-      let apu = TestsConstants.generateMdocGeneratedNonce()
       let verifiablePresentations: [VerifiablePresentation] = [
         .generic(TestsConstants.cbor)
       ]
@@ -293,7 +292,6 @@ final class DirectPostJWTTests: DiXCTest {
       
       // Obtain consent
       let submission = TestsConstants.presentationSubmission(presentationDefinition!)
-      let apu = TestsConstants.generateMdocGeneratedNonce()
       let verifiablePresentations: [VerifiablePresentation] = [
         .generic(TestsConstants.cbor)
       ]
@@ -585,7 +583,6 @@ final class DirectPostJWTTests: DiXCTest {
       
       // Obtain consent
       let submission = TestsConstants.testPresentationSubmission
-      let apu = TestsConstants.generateMdocGeneratedNonce()
       let verifiablePresentations: [VerifiablePresentation] = [
         .generic(TestsConstants.cbor)
       ]
@@ -717,7 +714,6 @@ final class DirectPostJWTTests: DiXCTest {
       
       // Obtain consent
       let submission = TestsConstants.testPresentationSubmissionSdJwt
-      let apu = TestsConstants.generateMdocGeneratedNonce()
       let verifiablePresentations: [VerifiablePresentation] = [
         .generic(presentation!)
       ]
@@ -836,7 +832,6 @@ final class DirectPostJWTTests: DiXCTest {
       
       // Obtain consent
       let submission = TestsConstants.presentationSubmission(presentationDefinition!)
-      let apu = TestsConstants.generateMdocGeneratedNonce()
       let verifiablePresentations: [VerifiablePresentation] = [
         .generic(TestsConstants.cbor)
       ]
@@ -944,7 +939,6 @@ final class DirectPostJWTTests: DiXCTest {
 
       // Obtain consent
       let submission = TestsConstants.testPresentationSubmission
-      let apu = TestsConstants.generateMdocGeneratedNonce()
       let verifiablePresentations: [VerifiablePresentation] = [
         .generic(TestsConstants.cbor)
       ]
@@ -1343,7 +1337,6 @@ final class DirectPostJWTTests: DiXCTest {
       definitionID: "psId",
       descriptorMap: []
     )
-    let apu = TestsConstants.generateMdocGeneratedNonce()
     let verifiablePresentations: [VerifiablePresentation] = [
       .generic(TestsConstants.cbor)
     ]
@@ -1442,7 +1435,7 @@ final class DirectPostJWTTests: DiXCTest {
     /// Copy the "Authenticate with wallet link", choose the value for "request_uri"
     /// Decode the URL online and paste it below in the url variable
     /// Note:  The url is only valid for one use
-    let url = "eudi-openid4vp://?client_id=x509_san_dns%3Adev.verifier-backend.eudiw.dev&request_uri=https%3A%2F%2Fdev.verifier-backend.eudiw.dev%2Fwallet%2Frequest.jwt%2Fu_d95_yQQbi6GULn_Yl-4z0AlxIucfKF4_EDNiZn8GzLcf0Evt5_-ko_ve27pVOCPQm6C5zbbnFOvkeTbLn7hw"
+    let url = "#09"
     
     overrideDependencies()
     let result = try? await sdk.authorize(
@@ -1477,6 +1470,100 @@ final class DirectPostJWTTests: DiXCTest {
           verifiablePresentations: verifiablePresentations,
           presentationSubmission: submission
         )
+      )
+      
+      // Generate a direct post authorisation response
+      let response = try? XCTUnwrap(AuthorizationResponse(
+        resolvedRequest: request,
+        consent: consent,
+        walletOpenId4VPConfig: wallet
+      ), "Expected item to be non-nil")
+      
+      // Dispatch
+      XCTAssertNotNil(response)
+      
+      let result: DispatchOutcome = try await sdk.dispatch(response: response!)
+      switch result {
+      case .accepted:
+        XCTAssert(true)
+      default:
+        XCTAssert(false)
+      }
+    }
+  }
+  
+  func testSDKEndtoEndWebVerifierDirectPostJwtX509DCQL() async throws {
+    
+    let rsaPrivateKey = try KeyController.generateRSAPrivateKey()
+    let rsaPublicKey = try KeyController.generateRSAPublicKey(from: rsaPrivateKey)
+    let privateKey = try KeyController.generateECDHPrivateKey()
+    
+    let rsaJWK = try RSAPublicKey(
+      publicKey: rsaPublicKey,
+      additionalParameters: [
+        "use": "sig",
+        "kid": UUID().uuidString,
+        "alg": "RS256"
+      ])
+    
+    let chainVerifier = { certificates in
+      let chainVerifier = X509CertificateChainVerifier()
+      let verified = try? chainVerifier.verifyCertificateChain(
+        base64Certificates: certificates
+      )
+      return chainVerifier.isChainTrustResultSuccesful(verified ?? .failure)
+    }
+    
+    
+    let keySet = try WebKeySet(jwk: rsaJWK)
+    let wallet: SiopOpenId4VPConfiguration = .init(
+      subjectSyntaxTypesSupported: [
+        .decentralizedIdentifier,
+        .jwkThumbprint
+      ],
+      preferredSubjectSyntaxType: .jwkThumbprint,
+      decentralizedIdentifier: try .init(rawValue: "did:example:123"),
+      signingKey: privateKey,
+      signingKeySet: keySet,
+      supportedClientIdSchemes: [
+        .x509SanDns(trust: chainVerifier)
+      ],
+      vpFormatsSupported: [],
+      jarConfiguration: .default,
+      vpConfiguration: VPConfiguration.default()
+    )
+    
+    let sdk = SiopOpenID4VP(walletConfiguration: wallet)
+    
+    /// To get this URL, visit https://verifier.eudiw.dev/
+    /// and  "Request for the entire PID"
+    /// Copy the "Authenticate with wallet link", choose the value for "request_uri"
+    /// Decode the URL online and paste it below in the url variable
+    /// Note:  The url is only valid for one use
+    let url = "#10"
+    
+    overrideDependencies()
+    let result = try? await sdk.authorize(
+      url: URL(
+        string: "eudi-wallet://authorize?client_id=\(TestsConstants.clientId)&request_uri=\(url)"
+      )!
+    )
+    
+    guard let result = result else {
+      XCTExpectFailure("this tests depends on a local verifier running")
+      XCTAssert(false)
+      return
+    }
+    
+    switch result {
+    case .notSecured: break
+    case .jwt(request: let request):
+      
+      // Obtain consent
+      let consent: ClientConsent = .vpToken(
+        vpContent: .dcql(verifiablePresentations: [
+          try QueryId(value: "query_0"): .generic(TestsConstants.cbor)
+        ])
       )
       
       // Generate a direct post authorisation response
