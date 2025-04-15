093f8a47983456fff58c1a8721cfdb0bb5a2aa49
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-04-15 10:36:51 +0300

    [fix] updated wallet metadata to support encryption metadata

--- Sources/WalletEntities/WalletMetaData.swift
+++ Sources/WalletEntities/WalletMetaData.swift
@@ -15,26 +15,60 @@
  */
 import Foundation
 import SwiftyJSON
+import JOSESwift
 
 // Function to create wallet metadata
-public func walletMetaData(cfg: SiopOpenId4VPConfiguration) -> JSON {
+public func walletMetaData(
+  cfg: SiopOpenId4VPConfiguration,
+  key: SecKey? = nil
+) -> JSON {
   var json = JSON()
   
-  // JAR related
-  json[REQUEST_OBJECT_SIGNING_ALG_VALUES_SUPPORTED] = JSON(cfg.jarConfiguration.supportedAlgorithms.map { $0.name })
+  json[REQUEST_OBJECT_SIGNING_ALG_VALUES_SUPPORTED] = JSON(
+    cfg.jarConfiguration.supportedAlgorithms.map { $0.name }
+  )
   
-  // OpenIdVP
-  json[PRESENTATION_DEFINITION_URI_SUPPORTED] = JSON(cfg.vpConfiguration.presentationDefinitionUriSupported)
+  json[PRESENTATION_DEFINITION_URI_SUPPORTED] = JSON(
+    cfg.vpConfiguration.presentationDefinitionUriSupported
+  )
   
   json[VP_FORMATS_SUPPORTED] = cfg.vpConfiguration.vpFormats.toJSON()["vp_formats"]
+  json[CLIENT_ID_SCHEMES_SUPPORTED] = JSON(
+    cfg.supportedClientIdSchemes.map { $0.name }
+  )
   
-  json[CLIENT_ID_SCHEMES_SUPPORTED] = JSON(cfg.supportedClientIdSchemes.map { $0.name })
-  
-  // Response types supported
   json[RESPONSE_TYPES_SUPPORTED] = JSON(["vp_token", "id_token"])
-  
-  // Response modes supported
   json[RESPONSE_MODES_SUPPORTED] = JSON(["direct_post", "direct_post.jwt"])
+  
+  if let options: PostOptions = cfg.jarConfiguration.supportedRequestUriMethods.isPostSupported() {
+    switch options.jarEncryption {
+    case .notRequired: break
+    case .required(
+      let encryptionRequirementSpecification
+    ):
+      if let key = key,
+         let publicKey = try? KeyController.generateECDHPublicKey(from: key),
+         let publicJwk = try? ECPublicKey(
+           publicKey: publicKey,
+           additionalParameters: [
+             "use": "enc",
+             "kid": UUID().uuidString,
+             "alg": encryptionRequirementSpecification.supportedEncryptionAlgorithm.rawValue
+           ]
+         ),
+         let jwkJson = try? JSON(["keys": [publicJwk.toDictionary()]]),
+         publicJwk["crv"] == encryptionRequirementSpecification.ephemeralEncryptionKeyCurve.rawValue {
+        
+        json[JWKS] = jwkJson
+        json[AUTHORIZATION_ENCRYPTION_ALG_VALUES_SUPPORTED] = JSON(
+          [encryptionRequirementSpecification.supportedEncryptionAlgorithm.rawValue]
+        )
+        json[AUTHORIZATION_ENCRYPTION_ENC_VALUES_SUPPORTED] = JSON(
+          [encryptionRequirementSpecification.supportedEncryptionMethod.rawValue]
+        )
+      }
+    }
+  }
   
   return json
 }
@@ -49,4 +83,18 @@ private let CLIENT_ID_SCHEMES_SUPPORTED = "client_id_schemes_supported"
 private let VP_FORMATS_SUPPORTED = "vp_formats_supported"
 private let RESPONSE_TYPES_SUPPORTED = "response_types_supported"
 private let RESPONSE_MODES_SUPPORTED = "response_modes_supported"
+private let JWKS = "jwks"
+private let CONTENT_TYPE_JWT = "JWT"
 
+private extension JWKSet {
+  func toJSON() -> JSON? {
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+
+    guard let data = try? encoder.encode(self) else {
+      return nil
+    }
+    
+    return JSON(data)
+  }
+}
