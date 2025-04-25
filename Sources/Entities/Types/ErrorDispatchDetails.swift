9e027c088c84f385e1913f8b4d9aa05bb48ba7cc
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-04-25 11:45:20 +0300

    [fix] response mode optional in dispatch details

--- Sources/Entities/Types/ErrorDispatchDetails.swift
+++ Sources/Entities/Types/ErrorDispatchDetails.swift
@@ -17,7 +17,7 @@ import Foundation
 import Foundation
 
 public struct ErrorDispatchDetails: Sendable {
-  public let responseMode: ResponseMode
+  public let responseMode: ResponseMode?
   public let nonce: String?
   public let state: String?
   public let clientId: VerifierId?
@@ -25,7 +25,7 @@ public struct ErrorDispatchDetails: Sendable {
   
   // Default initializer
   public init(
-    responseMode: ResponseMode,
+    responseMode: ResponseMode?,
     nonce: String? = nil,
     state: String? = nil,
     clientId: VerifierId?,
